72b24102817dea7c3d825d73fa37265585a93e21
Juanes1811 <juan.alzatequintero@upb.edu.co>
2023-03-27 13:43:33 -0500

    Rig Mikey

---
+++ Assets/Script/IKSnapper.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class IKSnapper : MonoBehaviour
+{
+    [SerializeField][Range(0,1)] private float proceduralInfluence;
+    [SerializeField] private MultiParentConstraint[] animatedBones;
+    [SerializeField] private MultiParentConstraint[] proceduralBones;
+
+    private void UpdateInfluence(float weight)
+    {
+        if (animatedBones == null) return;
+        
+        foreach (MultiParentConstraint multiParentConstraint in animatedBones)
+        {
+            if (multiParentConstraint == null) continue;
+            multiParentConstraint.weight = weight;
+        }
+
+        if (proceduralBones == null) return;
+        
+        foreach (MultiParentConstraint proceduralConstraint in proceduralBones)
+        {
+            if (proceduralConstraint == null) continue;
+            proceduralConstraint.weight = 1 - weight;
+        }
+    }
+
+    private void OnValidate()
+    {
+        UpdateInfluence(proceduralInfluence);
+    }
+}
